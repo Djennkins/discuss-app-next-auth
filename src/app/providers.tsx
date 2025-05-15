e139c6b8db5eb4a29493836d503ca19e8db90fca
Djennkins <djennkins228@gmail.com>
2025-05-15 10:08:47 +0300

    init NextUI

---
+++ src/app/providers.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { NextUIProvider } from '@nextui-org/react';
+
+interface ProvidersProps {
+    children: ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
+    return (
+        <NextUIProvider>
+            {children}
+        </NextUIProvider>
+    );
+}
