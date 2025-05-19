293f3cf7c4c8641d2c8f5e95fed94ea2156ed814
Djennkins <djennkins228@gmail.com>
2025-05-19 13:43:37 +0300

    handled database errors in form

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,7 +1,12 @@
 'use server';
 
+import type { Topic } from '@prisma/client';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
 import { auth } from '@/auth';
+import { db } from '@/db/index';
+import { revalidatePath } from 'next/cache';
+import paths from '@/path';
 
 const createTopicSchema = z.object({
     name: z.string().min(3).regex(/[a-z-]+$/, {
@@ -44,8 +49,32 @@ export async function createTopic(
         };
     }
 
-    //TODO: revalidate the homepage
-    return {
-        errors: {},
-    };
+    let topic: Topic;
+    try {
+        topic = await db.topic.create({
+            data: {
+                slug: result.data.name,
+                description: result.data.description,
+
+            },
+        });
+
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            return {
+                errors: {
+                    _form: [err.message],
+                },
+            };
+        } else {
+            return {
+                errors: {
+                    _form: ['Something went wrong'],
+                },
+            };
+        }
+    }
+
+    revalidatePath('/');
+    redirect(paths.topicShow(topic.slug));
 }
