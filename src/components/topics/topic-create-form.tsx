03247dccce1d36dc8678e772613f6e4374d86019
Djennkins <djennkins228@gmail.com>
2025-05-19 13:20:36 +0300

    handled general form errors

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -8,7 +8,6 @@ import {
     Popover,
     PopoverTrigger,
     PopoverContent,
-    Form,
 } from '@nextui-org/react';
 import * as actions from '@/actions';
 
@@ -50,6 +49,16 @@ export default function TopicCreateForm() {
                             isInvalid={!!formState.errors.description}
                             errorMessage={formState.errors.description?.join(', ')}
                         />
+                        {
+                            formState.errors._form
+                                ? (
+                                    <div
+                                        className="rounded p-2 bg-red-200 border border-red-400">
+                                        {formState.errors._form.join(', ')}
+                                    </div>
+                                )
+                                : null
+                        }
                         <Button type="submit">Submit</Button>
                     </div>
                 </form>
