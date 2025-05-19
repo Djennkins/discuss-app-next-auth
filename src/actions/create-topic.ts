e06581949ca99358e65b6dd53ddd141a29a668c0
Djennkins <djennkins228@gmail.com>
2025-05-19 07:48:11 +0300

    added our form state interface

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -9,7 +9,17 @@ const createTopicSchema = z.object({
     description: z.string().min(10),
 });
 
-export async function createTopic(formState: number, formData: FormData) {
+interface CreateTopicFormState {
+    errors: {
+        name?: string[];
+        description?: string[];
+    };
+}
+
+export async function createTopic(
+    formState: CreateTopicFormState,
+    formData: FormData,
+): Promise<CreateTopicFormState> {
     const result = createTopicSchema.safeParse(
         {
             name: formData.get('name'),
@@ -18,9 +28,13 @@ export async function createTopic(formState: number, formData: FormData) {
     );
 
     if (!result.success) {
-        console.log(result.error.flatten().fieldErrors);
+        return {
+            errors: result.error.flatten().fieldErrors,
+        };
     }
-    
+
     //TODO: revalidate the homepage
-    return 10;
+    return {
+        errors: {},
+    };
 }
